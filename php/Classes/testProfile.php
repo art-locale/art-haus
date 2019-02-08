f855fed40901f5381b01c2f0cebbc5c2a508d136
William Isengard <william.isengard@gmail.com>
2019-02-08 09:14:49 -0700

    Added PHPUnit database connection interface and assembled tables https://app.asana.com/0/1108331976344856/1108331976344858

--- php/Classes/testProfile.php
+++ php/Classes/testProfile.php
@@ -1,17 +1,17 @@
 <?php
 namespace ArtLocale\ArtHaus;
 
-use art-haus\sql\ddl-art-locale{Profile};
-use Ramsey\Uuid\Uuid;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+use PHPUnit\DbUnit\DataSet\QueryDataSet;
+use PHPUnit\DbUnit\Database\Connection;
+use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
 
 // grab the encrypted properties file
 require_once("/etc/apache2/capstone-mysql/Secret.php");
 
-// grab the class under scrutiny
-require_once(dirname(__DIR__) . "/autoload.php");
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 
-// grab the uuid generator
-require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /**
  * Full PHPUnit test for the Profile class
@@ -23,3 +23,28 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * @author William Isengard william.isengard@gmail.com>
  **/
 
+ abstract class DataDesignTest extends TestCase {
+ 	use TestCaseTrait;
+ 
+ 	/**
+ 	 * PHPUnit database connection interface
+ 	 * @var Connection $connection
+ 	 **/
+ 	protected $connection = null;
+
+ 	/**
+ 	 * assembles the table from the schema and provides it to PHPUnit
+ 	 *
+ 	 * @return QueryDataSet assembled schema for PHPUnit
+ 	 **/
+ 	public final function getDataSet() : QueryDataSet {
+ 		$dataset = new QueryDataSet($this->getConnection());
+
+ 		// add all the tables for the project here
+ 		// THESE TABLES *MUST* BE LISTED IN THE SAME ORDER THEY WERE CREATED!!!!
+ 		$dataset->addTable("profile");
+ 		$dataset->addTable("gallery");
+    $dataset->addTable("image");
+    $dataset->addTable("applaud");
+ 		return($dataset);
+ 	}
