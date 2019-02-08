1faef623952f71144ea36b1ed7c9d6e9fbfd2b04
William Isengard <william.isengard@gmail.com>
2019-02-08 10:17:38 -0700

    Defined ProfileTest class and defined variables https://app.asana.com/0/1108331976344856/1108331976344858

--- php/Classes/TestProfile.php
+++ php/Classes/TestProfile.php
@@ -18,3 +18,68 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * @see Profile
  * @author William Isengard <william.isengard@gmail.com>
  **/
+
+ class ProfileTest extends ArtHausTest {
+ 	/**
+ 	 * Art Haus user profile
+ 	 * @var Profile profile
+ 	 **/
+ 	protected $profile = null;
+
+ 	/**
+ 	 * id for this profile
+ 	 * @var Uuid $VALID_PROFILEID
+ 	 */
+ 	protected $VALID_PROFILEID;
+
+ 	/**
+ 	 * Activation token for initial profile creation
+ 	 * @var string $VALID_PROFILEACTIVATIONTOKEN
+ 	 **/
+ 	protected $VALID_PROFILEACTIVATIONTOKEN = "PHPUnit test passing";
+
+ 	/**
+ 	 * Date and time profile was created; this starts as null and is assigned later
+ 	 * @var \DateTime $VALID_PROFILEDATE
+ 	 **/
+ 	protected $VALID_PROFILEDATE = null;
+
+ 	/**
+ 	 * Valid timestamp to use as sunriseProfileDate
+ 	 */
+ 	protected $VALID_SUNRISEDATE = null;
+
+ 	/**
+ 	 * Valid timestamp to use as sunsetProfileDate
+ 	 */
+ 	protected $VALID_SUNSETDATE = null;
+
+  /**
+   * Email address for profile owner
+   * @var string $VALID_PROFILEEMAIL
+   **/
+  protected $VALID_PROFILEEMAIL = "PHPUnit test still passing";
+
+  /**
+   * Location of profile owner
+   * @var string $VALID_PROFILELOCATION
+   **/
+  protected $VALID_PROFILELOCATION = "PHPUnit test still passing";
+
+  /**
+   * Name of profile owner
+   * @var string $VALID_PROFILENAME
+   **/
+  protected $VALID_PROFILENAME = "PHPUnit test still passing";
+
+  /**
+   * Hash of profile owner account password
+   * @var string $VALID_PROFILEPASSWORD
+   **/
+  protected $VALID_PROFILEPASSWORD = "PHPUnit test still passing";
+
+  /**
+   * Website of profile owner
+   * @var string $VALID_PROFILEWEBSITE
+   **/
+  protected $VALID_PROFILEWEBSITE = "PHPUnit test still passing";
