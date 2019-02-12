f2ee4fefc3533d69806290a40827a4fbf3478c14
William Isengard <william.isengard@gmail.com>
2019-02-12 16:10:36 -0700

    Added null options on date and activation token in Profile constructor https://app.asana.com/0/1108331976344856/1108331976344858

--- php/Classes/Profile.php
+++ php/Classes/Profile.php
@@ -38,12 +38,12 @@ class Profile implements \JsonSerializable {
   private $profileEmail;
   /**
   * Latitude location of profile owner
-  * @var string $profileLatitude;
+  * @var float $profileLatitude;
   **/
   private $profileLatitude;
   /**
   * Longitude location of profile owner
-  * @var string $profileLongitude;
+  * @var float $profileLongitude;
   **/
 	private $profileLongitude;
 	/**
@@ -78,7 +78,7 @@ class Profile implements \JsonSerializable {
    * @throws \Exception if some other exception occurs
    * @Documentation https://php.net/manual/en/language.oop5.decon.php
    **/
-	 public function __construct($newProfileId, string $newProfileActivationToken, string $newProfileDate, string $newProfileEmail, float $newProfileLatitude, float $newProfileLongitude, string $newProfileName, string $newProfilePassword, string $newProfileWebsite) {
+	 public function __construct($newProfileId, ?string $newProfileActivationToken, ?string $newProfileDate, string $newProfileEmail, float $newProfileLatitude, float $newProfileLongitude, string $newProfileName, string $newProfilePassword, string $newProfileWebsite) {
 		 try {
 			 	$this->setProfileId($newProfileId);
 			 	$this->setProfileActivationToken($newProfileActivationToken);
