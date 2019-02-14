0d3df02972a790cf865b72bfd6ee8120e3d12b80
Brandon Huffman <bt_huffman@msn.com>
2019-02-14 10:58:11 -0700

    In TestImage.php:finished developing function to insert a new Image into database and updating it. Adjusted several DateTime related code (timestamp). Started the delete image test. In Image.php: NONE https://app.asana.com/0/1108331976344856/1108331976344860. DDL.SQL: NONE https://app.asana.com/0/1108331976344856/1108331976344859

--- php/Classes/Tests/TestImage.php
+++ php/Classes/Tests/TestImage.php
@@ -21,43 +21,44 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 class TestImage extends ArtHausTest {
 	/**
-	 * valid Art Haus user image ]
-	 * @var Image image
+	 * Profile that create image; for foreign key relations
+	 * @var Profile profile
 	 **/
-	protected $image = null;
+	protected $profile = null;
 
 	/**
-	 * id for this imageId
-	 * @var Uuid $VALID_IMAGEID
+	 * valid profile password to create profile object to own the test
+	 * @var VALID_HASH
 	 */
-	protected $VALID_IMAGEID;
+	protected $VALID_PROFILE_HASH;
 
-	/**
+	/** //FIXME don't see example of this in documentation
 	 * placeholder image gallery id for initial image creation
 	 * @var string $VALID_IMAGEGALLERYID
 	 **/
-	protected $VALID_IMAGEGALLERYID;
+//	protected $VALID_IMAGEGALLERYID;
 
-	/**
+	/**	/** //FIXME don't see example of this in documentation
 	 * placeholder image profile id for initial image creation
 	 * @var string $VALID_IMAGEPROFILEID
 	 **/
-	protected $VALID_IMAGEPROFILEID;
+//	protected $VALID_IMAGEPROFILEID;
+
 	/**
 	 * Date image was created- this starts as null and is assigned later
 	 * @var \Date $VALID_IMAGEDATE
 	 **/
-	protected $VALID_IMAGEDATE;
+	protected $VALID_IMAGEDATE = null;
 
 	/**
 	 * Valid timestamp to use as sunriseImageDate
 	 */
-	protected $VALID_SUNRISEDATE = null;
+	protected $VALID_SUNRISEDATE = null; //FIXME Necessary?
 
 	/**
 	 * Valid timestamp to use as sunsetImageDate
 	 */
-	protected $VALID_SUNSETDATE = null;
+	protected $VALID_SUNSETDATE = null; //FIXME Necessary?
 
 	/**
 	 * valid title for image title
@@ -65,12 +66,11 @@ class TestImage extends ArtHausTest {
 	 **/
 	protected $VALID_IMAGETITLE = "test test 123";
 
-
 	/**
 	 * updated image title
 	 * @var string $VALID_IMAGETITLE
 	 **/
-	protected $VALID_IMAGETITLE = "John Doe";
+	protected $VALID_IMAGETITLE2 = "John Doe";
 
 	/**
 	 * valid url of image url
@@ -82,7 +82,7 @@ class TestImage extends ArtHausTest {
 	 * valid url of image url
 	 * @var string $VALID_IMAGEURL2
 	 **/
-	protected $VALID_IMAGEURL2 = null;
+	protected $VALID_IMAGEURL = null;
 
 
 	/**
@@ -90,28 +90,8 @@ class TestImage extends ArtHausTest {
 	 * @var string $VALID_IMAGEURL2
 	 **/
 	protected $VALID_IMAGEURL2 = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Statue_of_Liberty_7.jpg/250px-Statue_of_Liberty_7.jpg";
-
-	/**
-	 * updated hash of profile owner account password
-	 * @var string $VALID_PROFILEPASSWORD2
-	 **/
-	protected $VALID_PROFILEPASSWORD2;
-
-	/**
-	 * website of profile owner
-	 * @var string $VALID_PROFILEWEBSITE
-	 **/
-	protected $VALID_PROFILEWEBSITE = "www.etsy.com";
-
-	/**
-	 * updated website of profile owner
-	 * @var string $VALID_PROFILEWEBSITE2
-	 **/
-	protected $VALID_PROFILEWEBSITE2 = "www.linkedin.com";
-
 	/***********************************************************************************************************************
-	 * START OF OPERATIONS
-	 *****************************************************************************************************************/
+	 * START OF OPERATIONS*****************************************************************************************************************/
 
 	/** FIXME Ask george if need to change any of the components here
 	 * setup operation to create hash and salt.
@@ -120,27 +100,28 @@ class TestImage extends ArtHausTest {
 		parent::setUp();
 		//
 		$password = "password1234";
-		$this->VALID_PROFILEPASSWORD = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+		$this->VALID_PROFILE_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
 		$this->VALID_PROFILEACTIVATIONTOKEN = bin2hex(random_bytes(16));
 
-		$this->VALID_PROFILEDATE = new \DateTime();
+		//create and insert a Profile to own the test Image
+		$this->profile = new Profile(generateUuidV4(), null, null,"@handle", 89.1234, 100.098, "this title", $this->VALID_PROFILE_HASH, null); //FIXME ProfilePassword?
+$this->profile->insert($this->getPDO());
 
-		//format the sunrise date to use for testing
+		// calculate the date (just use the time the unit test was setup)
+		$this->VALID_TIMESTAMP = new \DateTime();
+
+		//format the sunrise date to use for testing //FIXME Necessary?
 		$this->VALID_SUNRISEDATE = new \DateTime();
 		$this->VALID_SUNRISEDATE->sub(new \DateInterval("P10D"));
 
-		//format the sunset date to use for testing
+		//format the sunset date to use for testing //FIXME Necessary?
 		$this->VALID_SUNSETDATE = new\DateTime();
 		$this->VALID_SUNSETDATE->add(new \DateInterval("P10D"));
 	}
 
-	//Fixme What of this?
-	//     // calculate the date (just use the time the unit test was setup...)
-	//   $this->VALID_IMAGEDATE = new \DateTime();
-
 	/****************************************************************************************************************
-	 * TEST CREATING A VALID IMAGE
-	 **************************************************************************************************************/
+	 * TEST CREATING A VALID IMAGE**************************************************************************************************************/
+
 	public function testCreateImage() : void {
 
 		// count the number of rows and save it for later
@@ -149,7 +130,7 @@ class TestImage extends ArtHausTest {
 		// create a new Image and insert into database
 		$imageId = generateUuidV4();
 
-		image = new Image ($imageId, $this->VALID_IMAGEGALLERYID, $this->VALID_IMAGEPROFILEID, $this->VALID_IMAGEDATE,$this->VALID_IMAGETITLE, $this->VALID_IMAGEURL);
+		$image = new Image ($imageId, $this->VALID_IMAGEGALLERYID, $this->VALID_IMAGEPROFILEID, $this->VALID_IMAGEDATE,$this->VALID_IMAGETITLE, $this->VALID_IMAGEURL);
 		$image->insert($this->getPDO());
 		// grab the data from mySQL and enforce the fields match expectations
 		$pdoImage = Image::getImageByImageId($this->getPDO(), $image->getImageId());
@@ -162,10 +143,10 @@ class TestImage extends ArtHausTest {
 		$this->assertEquals($pdoImage->getImageUrl(),$this->VALID_IMAGEURL);
 	}
 
-	 /**
-	  * test inserting an image and updating it
-	  **/
-	 public function testUpdateImage() {
+	/****************************************************************************************************************
+	 * TEST INSERTING AN IMAGE AND UPDATING IT**************************************************************************************************************/
+
+	 public function testUpdateImage() : void {
 
 	 	// count the number of rows and save it for later
 	 	$numRows = $this->getConnection()->getRowCount("image");
@@ -176,55 +157,37 @@ class TestImage extends ArtHausTest {
 	 	$image->insert($this->getPDO());
 
 	 	// edit the Image and update it in mySQL
-	 	$image->setImageEmail($this->$VALID_PROFILEEMAIL2);
-	 	$profile->update($this->getPDO());
-	 	// $profile->setProfileLatitude($this->$VALID_PROFILELatitude);
-	 	// $profile->setProfileLongitude($this->$VALID_PROFILELongitude);
-	 	// $profile->setProfileName($this->$VALID_PROFILENAME2);
-	 	// $profile->setProfilePassword($this->VALID_PROFILEPASSWORD2);
-	 	// $profile->setProfileWebsite($this->$VALID_PROFILEEMAIL2);
+		 $image->setImageTitle($this->$VALID_IMAGETITLE2);
+		 $image->setImageUrl($this->VALID_IMAGEURL2);
+	 	$image->update($this->getPDO());
 
-	 	// access the data from database and confirm the data matches expectations
-	 	$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
-
-	   $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
-	 	$this->assertEquals($pdoProfile->getProfileId(), $profileId);
-	 	$this->assertEquals($pdoProfile->getProfileActivationToken(), $this->VALID_PROFILEACTIVATIONTOKEN);
-	 	$this->assertEquals($pdoProfile->getProfileDate(), $this->VALID_PROFILEDATE);
-	 	$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_PROFILEEMAIL2);
-	 	$this->assertEquals($pdoProfile->getProfileLatitude(), $this->VALID_PROFILELATITUDE);
-	  	$this->assertEquals($pdoProfile->getProfileLongitude(), $this->VALID_PROFILELONGITUDE);
-	 	$this->assertEquals($pdoProfile->getProfileName(), $this->VALID_PROFILENAME);
-	 	$this->assertEquals($pdoProfile->getProfilePassword(), $this->VALID_PROFILEPASSWORD);
-	 	$this->assertEquals($pdoProfile->getProfileWebsite(), $this->VALID_PROFILEWEBSITE);
+	 	//grab the data from mySQL and enforce the fields match expectations
+		$pdoImage = Image::getImageByGalleryId($this->getPDO(), $image->getImageId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("image"));
+		$this->assertEquals($pdoImage->getImageId(), $imageId);
+		$this->assertEquals($pdoImage->getImageGalleryId(), $this->gallery->getGalleryId); //FIXME?
+		$this->assertEquals($pdoImage->getImageProfileId(), $this->profile->getProfileId); //FIXME
+		 $this->assertEquals($pdoImage->getImageDate()->getTimestamp(), $this->VALID_IMAGEDATE->getTimestamp());
+		$this->assertEquals($pdoImage->getImageTitle(), $this->$VALID_IMAGETITLE2);
+		$this->assertEquals($pdoImage->getImageUrl(), $this->$VALID_IMAGEURL2);
 	 }
 
-	// /**
-	//  * test to create a profile and delete it
-	//  **/
-	// public function testDeleteProfile() : void {
-	//   // count the number of rows and save it for later
-	//   $numRows = $this->getConnection()->getRowCount("profile");
-	//   // create a new profile and insert into database
-	//   $profileId = generateUuidV4();
-	//   $profile = new Profile($profileId, $this->VALID_PROFILEACTIVATIONTOKEN, $this->VALID_PROFILEDATE, $this->VALID_PROFILEEMAIL, $this->VALID_PROFILELATITUDE, $this->VALID_PROFILELONGITUDE, $this->VALID_PROFILENAME, $this->VALID_PROFILEPASSWORD, $this->VALID_PROFILEWEBSITE);
-	//   $profile->insert($this->getPDO());
-	//   // delete the profile from database
-	//   $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
-	//   $profile->delete($this->getPDO());
-	//   // access database and confirm profile deleted
-	//   $pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
-	//   $this->assertNull($pdoProfile);
-	//   $this->assertEquals($numRows, $this->getConnection()->getRowCount("profile"));
-	// }
-	//
+	 /**
+	  * test to create a image and delete it
+	  **/
+	 public function testDeleteImage() : void {
+	   // count the number of rows and save it for later
+	   $numRows = $this->getConnection()->getRowCount("image");
+	   // create a new image and insert into database
+		 $imageId = generateUuidV4();
+		 $image = new Image($imageId, $this->VALID_IMAGEGALLERYID, $this->VALID_IMAGEPROFILEID, $this->VALID_IMAGEDATE,$this->VALID_IMAGETITLE, $this->VALID_IMAGEURL);
+		 $image->insert($this->getPDO());
+	   // delete the image from database
+	   $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("image"));
+	   $image->delete($this->getPDO());
+	   // access database and confirm image deleted
+	   $pdoImage = Image::getImageByImageId($this->getPDO(), $image->getImageId());
+	   $this->assertNull($pdoImage);
+	   $this->assertEquals($numRows, $this->getConnection()->getRowCount("profile"));
+	 }
 
-	//
-	//   //format the sunrise date to use for testing
-	//   $this->VALID_SUNRISEDATE = new \DateTime();
-	//   $this->VALID_SUNRISEDATE->sub(new \DateInterval("P10D"));
-	//
-	//   //format the sunset date to use for testing
-	//   $this->VALID_SUNSETDATE = new\DateTime();
-	//   $this->VALID_SUNSETDATE->add(new \DateInterval("P10D"));
-}
