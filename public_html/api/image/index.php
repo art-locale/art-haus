68e45b0affeef430b803053f706d1bf5d91bdb2f
Brandon Huffman <bt_huffman@msn.com>
2019-02-25 13:35:35 -0700

    Apart from two todo items involving cloudinary seemed to have completed the image API index pending testing. https://app.asana.com/0/1108331976344856/1111336939366882

--- public_html/api/image/index.php
+++ public_html/api/image/index.php
@@ -25,11 +25,12 @@ try {
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort23/arthaus.ini");
 	//determine which HTTP method is being used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
-	$tweetId = filter_input(INPUT_GET, "tweetId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileId = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	// sanitize input
+	$galleryId = filter_input(INPUT_GET, "galleryId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileId = filter_input(INPUT_GET, "ProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$config = readConfig("/etc/apache2/capstone-mysql/ddctwitter.ini");
-	$cloudinary = json_decode($config["cloudinary"]);
+	$config = readConfig("/etc/apache2/capstone-mysql/cohort23/arthaus.ini");
+	$cloudinary = json_decode($config["cloudinary"]); /*TODO*/
 	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
 	// process GET requests
 	if($method === "GET") {
@@ -39,9 +40,9 @@ try {
 		if(empty($id) === false) {
 			$image = Image::getImageByImageId($pdo, $id);
 		} elseif(empty($tweetId) === false) {
-			$reply->data = Image::getImageByImageTweetId($pdo, $tweetId)->toArray();
+			$reply->data = Image::getImageByImageGalleryId($pdo, $tweetId)->toArray();
 		} elseif(empty($profileId) === false) {
-			$reply->data = Image::getImageByProfileId($pdo, $profileId)->toArray();
+			$reply->data = Image::getImageByImageProfileId($pdo, $profileId)->toArray();
 		}
 	} elseif($method === "DELETE") {
 		//enforce that the end user has a XSRF token.
@@ -53,13 +54,13 @@ try {
 		}
 		//enforce the user is signed in and only trying to edit their own image
 		// use the image id to get the tweet id to get the profile id to compare it to the session profile id
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== Tweet::getTweetByTweetId($pdo, $image->getImageTweetId())->getTweetProfileId()) {
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== Tweet::getGalleryByGalleryId($pdo, $image->getImageGalleryId())->getGallerybyProfileId()) {
 			throw(new \InvalidArgumentException("You are not allowed to delete this image", 403));
 		}
 		//enforce the end user has a JWT token
 		validateJwtHeader();
 		// delete image from cloudinary
-		$cloudinaryResult = \Cloudinary\Uploader::destroy($image->getImageCloudinaryToken());
+		$cloudinaryResult = \Cloudinary\Uploader::destroy($image->getImageCloudinaryToken()); /*TODO*/
 		// delete image database
 		$image->delete($pdo);
 		// update reply
@@ -71,7 +72,7 @@ try {
 		if(empty($_SESSION["profile"]) === true) {
 			throw (new \InvalidArgumentException("you must be logged in to post images", 401));
 			// verify user is logged into the profile posting the tweet before uploading an image
-		} elseif($_SESSION["profile"]->getProfileId() !== Tweet::getTweetByTweetId($pdo, $tweetId)->getTweetProfileId()) {
+		} elseif($_SESSION["profile"]->getProfileId() !== Tweet::getGalleryByGalleryId($pdo, $galleryId)->getGalleryProfileId()) {
 			throw(new \InvalidArgumentException("You are not allowed to post an image to someone else's tweet", 403));
 		}
 		// assigning variable to the user profile, add image extension
@@ -79,7 +80,7 @@ try {
 		// upload image to cloudinary and get public id
 		$cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 500, "crop" => "scale"));
 		// after sending the image to Cloudinary, create a new image
-		$image = new Image(generateUuidV4(), $tweetId, $cloudinaryResult["signature"], $cloudinaryResult["secure_url"]);
+		$image = new Image(generateUuidV4(), $galleryId, $cloudinaryResult["signature"], $cloudinaryResult["secure_url"]);
 		$image->update($pdo);
 		// update reply
 		$reply->message = "Image uploaded Ok";
