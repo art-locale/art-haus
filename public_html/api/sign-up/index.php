86e6cd9885288ed6577db7bdb7b17aedacddaa37
William Isengard <william.isengard@gmail.com>
2019-02-27 11:38:11 -0700

    Updated secrets connection. https://app.asana.com/0/1108331976344856/1110701114697994

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -21,7 +21,8 @@ $reply->status = 200;
 $reply->data = null;
 try {
 	//grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort23/arthaus.ini");
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort23/arthaus.ini");
+  $pdo = $secrets->getPdoObject();
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 	if($method === "POST") {
