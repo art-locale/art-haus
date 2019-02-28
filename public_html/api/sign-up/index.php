56a24b25198b52a0c227d746b0c0e3685b7c8e1f
William Isengard <william.isengard@gmail.com>
2019-02-28 08:24:52 -0700

    Updated composer.json and sign-up.json snippet. Sign-up API tested successfully. https://app.asana.com/0/1108331976344856/1110701114697994

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -37,14 +37,14 @@ try {
 		if(empty($requestObject->profileEmail) === true) {
 			throw(new \InvalidArgumentException ("No profile email present", 405));
 		}
-    //profile email is a required field
-    if(empty($requestObject->profileLatitude) === true) {
-      throw(new \InvalidArgumentException ("No latitude present", 405));
-    }
-    //profile email is a required field
-    if(empty($requestObject->profileLongitude) === true) {
-      throw(new \InvalidArgumentException ("No longitude present", 405));
-    }
+    // //profile email is a required field
+    // if(empty($requestObject->profileLatitude) === true) {
+    //   throw(new \InvalidArgumentException ("No latitude present", 405));
+    // }
+    // //profile email is a required field
+    // if(empty($requestObject->profileLongitude) === true) {
+    //   throw(new \InvalidArgumentException ("No longitude present", 405));
+    // }
 		//verify that profile password is present
 		if(empty($requestObject->profilePassword) === true) {
 			throw(new \InvalidArgumentException ("Must input valid password", 405));
@@ -70,8 +70,10 @@ try {
 		}
 		$hash = password_hash($requestObject->profilePassword, PASSWORD_ARGON2I, ["time_cost" => 384]);
 		$profileActivationToken = bin2hex(random_bytes(16));
-		//create the profile object and prepare to insert into the database
-		$profile = new Profile(generateUuidV4(), $profileActivationToken, $requestObject->profileDate, $requestObject->profileEmail, $requestObject->profileLatitude, $requestObject->profileLongitude, $requestObject->profileName, $hash, "null");
+    $profileLatitude = "75.555555"; //TODO update to include generate function if possible//
+    $profileLongitude = "50.555555"; //TODO update to include generate function if possible//
+    //create the profile object and prepare to insert into the database
+		$profile = new Profile(generateUuidV4(), $profileActivationToken, $requestObject->profileDate, $requestObject->profileEmail, $profileLatitude, $profileLongitude, $requestObject->profileName, $hash, "null");
 		//insert the profile into the database
 		$profile->insert($pdo);
 		//compose the email message to send with th activation token
