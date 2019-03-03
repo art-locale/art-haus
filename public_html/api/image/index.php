7b4d6f2617c71f8dbe3b2df364c8ba0bb2e6269a
William Isengard <william.isengard@gmail.com>
2019-03-02 21:56:42 -0700

    Uncommented cloudinary functions. API looks good compared to other examples, need to figure out how to test. https://app.asana.com/0/1108331976344856/1111336939366882

--- public_html/api/image/index.php
+++ public_html/api/image/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/Classes/autoload.php";
-require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
@@ -31,9 +30,9 @@ try {
 	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$galleryId = filter_input(INPUT_GET, "galleryId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$profileId = filter_input(INPUT_GET, "ProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	// $config = $secrets->getSecret("cloudinary");
-	// $cloudinary = json_decode($config["cloudinary"]); /*TODO*/
-	// \Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
+	$config = $secrets->getSecret("cloudinary");
+	$cloudinary = json_decode($config["cloudinary"]);
+	\Cloudinary::config(["cloud_name" => $cloudinary->cloudName, "api_key" => $cloudinary->apiKey, "api_secret" => $cloudinary->apiSecret]);
 	// process GET requests
 	if($method === "GET") {
 		// set XSRF token
@@ -55,14 +54,14 @@ try {
 			throw(new RuntimeException("Image does not exist", 404));
 		}
 		//enforce the user is signed in and only trying to edit their own image
-		// use the image id to get the tweet id to get the profile id to compare it to the session profile id
+		// use the image id to get the imageProfile id to compare it to the session profile id
 		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== Gallery::getGalleryByGalleryId($pdo, $image->getImageGalleryId())->getGallerybyProfileId()) {
 			throw(new \InvalidArgumentException("You are not allowed to delete this image", 403));
 		}
 		//enforce the end user has a JWT token
 		validateJwtHeader();
 		// delete image from cloudinary
-		// $cloudinaryResult = \Cloudinary\Uploader::destroy($image->getImageCloudinaryToken()); /*TODO*/
+		$cloudinaryResult = \Cloudinary\Uploader::destroy($image->getImageCloudinaryToken());
 		// delete image database
 		$image->delete($pdo);
 		// update reply
@@ -73,16 +72,16 @@ try {
 		// verify the user is logged in
 		if(empty($_SESSION["profile"]) === true) {
 			throw (new \InvalidArgumentException("you must be logged in to post images", 401));
-			// verify user is logged into the profile posting the tweet before uploading an image
+			// verify user is logged into the profile before uploading an image
 		} elseif($_SESSION["profile"]->getProfileId() !== Gallery::getGalleryByGalleryId($pdo, $galleryId)->getGalleryProfileId()) {
 			throw(new \InvalidArgumentException("You cannot post images to someone else's profile", 403));
 		}
 		// assigning variable to the user profile, add image extension
 		$tempUserFileName = $_FILES["image"]["tmp_name"];
 		// upload image to cloudinary and get public id
-		// $cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 500, "crop" => "scale"));
+		$cloudinaryResult = \Cloudinary\Uploader::upload($tempUserFileName, array("width" => 500, "crop" => "scale"));
 		// after sending the image to Cloudinary, create a new image
-		// $image = new Image(generateUuidV4(), $galleryId, $cloudinaryResult["signature"], $cloudinaryResult["secure_url"]);
+		$image = new Image(generateUuidV4(), $galleryId, $cloudinaryResult["signature"], $cloudinaryResult["secure_url"]);
 		$image->update($pdo);
 		// update reply
 		$reply->message = "Image uploaded successfully";
