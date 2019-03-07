6bf1cbf19f47ca54ca2b9625721d26a70f44be6a
William Isengard <william.isengard@gmail.com>
2019-03-07 13:27:13 -0700

    Added art card, head, navbar, and sign-in home static ui pages

---
+++ static-ui/art-card.php
@@ -0,0 +1,15 @@
+<?php require_once ("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<main>
+    <div class="container-fluid p-2">
+        <div class="card">
+            <img class="card-img-top p-1" src="../src/img/test.jpg" alt="Card image cap">
+            <ul class="p-1 list-group list-group-flush">
+                <li class="list-group-item">Title: </li>
+                <li class="list-group-item">Artist: </li>
+            </ul>
+        </div>
+    </div>
+</main>
---
+++ static-ui/head-utils.php
@@ -0,0 +1,23 @@
+<head>
+
+  <!-- meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
+
+  <!-- Fontawesome CSS -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+
+  <!-- custom CSS stylesheet -->
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+
+  <!-- Bootstrap Files: jQuery, Popper.js, and Bootstrap JS -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+	<title>Art Haus</title>
+
+</head>
