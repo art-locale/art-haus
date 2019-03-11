ebe5dc82317841d340cc8354d1dc5132c1318653
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-11 11:37:05 -0600

    Added sample image to card. https://app.asana.com/0/1108331976344856/1113629340275113

--- static-ui/art-card.php
+++ static-ui/art-card.php
@@ -8,11 +8,12 @@
 
   <div class="container-fluid">
     <div class="card">
-      <img class="card-img-top" src="../src/img/test.jpg" alt="Card image cap" />
+      <img class="card-img-top" src="../sample-images/sample-image-4.jpg" alt="Card image cap" />
       <ul class="list-group list-group-flush">
         <li class="list-group-item">Title: </li>
         <li class="list-group-item">Artist: </li>
       </ul>
+       <a class="btn btn-lg" id="applaud" href="" role="button">Applaud</a>
     </div>
   </div>
 
