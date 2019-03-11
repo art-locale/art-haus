027acf2ba092aad6f6b5f9589b885e2478dc9293
William Isengard <william.isengard@gmail.com>
2019-03-11 11:55:44 -0600

    Updated footer links and styling.

--- static-ui/footer.php
+++ static-ui/footer.php
@@ -5,23 +5,23 @@
 
 <main>
 
-  <div class="fixed-bottom row py-5 py-md-3 py-sm-1 text-white text-center" id="footerRow">
+  <div class="container-fluid">
+  <div class="fixed-bottom row py-5 py-md-3 py-sm-1 text-white text-center bg-dark" id="footerRow">
     <div class="col">
       <div class="row">
         <div class="col-md-3 col-sm-4 col-3 mx-auto py-md-2 py-sm-3">
-          <a href="" target=”_blank”>
-            <img class="fab fa-2x fa-facebook-square" src="" alt="Facebook (Image courtesy of facebook.com)" title="Facebook (Image courtesy of facebook.com)" />
+          <a href="" target=”_blank” class="fab fa-2x fa-facebook-square" alt="Facebook (Image courtesy of facebook.com)" title="Facebook (Image courtesy of facebook.com)" >
           </a>
         </div>
         <div class="col-md-3 col-sm-4 col-3 mx-auto py-md-2 py-sm-3">
-          <a href="" target=”_blank”>
-            <img class="fab fa-2x fa-twitter" src="" alt="Twitter (Image courtesy of twitter.com)" title="Twitter (Image courtesy of twitter.com)" />
+          <a href="" target=”_blank” class="fab fa-2x fa-twitter" alt="Twitter (Image courtesy of twitter.com)" title="Twitter (Image courtesy of twitter.com)" >
           </a>
         </div>
       </div>
       <footer>Copyright Art Haus 2019</footer>
   	</div>
   </div>
+</div>
 
 </main>
 
