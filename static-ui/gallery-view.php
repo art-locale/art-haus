f80a2c7244d70b80583293aaaacf3d7ed27c87ff
Brandon Huffman <bt_huffman@msn.com>
2019-03-11 11:28:02 -0600

    Added the gallery-view and profile-view files along with some sample picture to test the carousel.

---
+++ static-ui/gallery-view.php
@@ -0,0 +1,17 @@
+<?php require_once("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<?php require_once("footer.php");?>
+
+
+<main>
+
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+			<h1 class="display-4">GALLERY NAME</h1>
+			<p class="lead">Profile Name</p>
+		</div>
+	</div>
+
+</main>
