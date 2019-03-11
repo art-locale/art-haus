ff635b8bb9d939672222aa243b36290d1853a128
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-11 17:00:12 -0600

    Adjusted colors on carousel. https://app.asana.com/0/1108331976344856/1113629340275111

--- static-ui/profile-view.php
+++ static-ui/profile-view.php
@@ -16,25 +16,25 @@
 <div class="carousel-bar containter-fluid">
 
    <!--CAROUSEL:-->
-	<div id="carousel" class="carousel slide bg-secondary" data-ride="carousel">
+	<div id="carousel" class="carousel slide bg-dark" data-ride="carousel">
 		<div class="carousel-inner">
          <!--GALLERY 1:-->
 			<div class="carousel-item active">
-				<img class="d-block w-100" src="../sample-images/sample-image-1.jpg" alt="First slide">
+				<img class="d-block w-100 my-3" src="../sample-images/sample-image-1.jpg" alt="First slide">
 				  	<div class="carousel-caption bg-dark">
 						<h5>Gallery Name</h5>
 					</div>
 			</div>
          <!--GALLERY 2:-->
 			<div class="carousel-item">
-				<img class="d-block w-100" src="../sample-images/sample-image-2.jpg" alt="Second slide">
+				<img class="d-block w-100 my-3" src="../sample-images/sample-image-2.jpg" alt="Second slide">
 					<div class="carousel-caption bg-dark">
 						<h5>Gallery Name</h5>
 					</div>
 			</div>
          <!--GALLERY 3:-->
 			<div class="carousel-item">
-				<img class="d-block w-100" src="../sample-images/sample-image-3.jpg" alt="Third slide">
+				<img class="d-block w-100 my-3" src="../sample-images/sample-image-3.jpg" alt="Third slide">
 					<div class="carousel-caption bg-dark ">
 						<h5>Gallery Name</h5>
 					</div>
