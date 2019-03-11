9ecb551741cda0ae23be6fc2f0837ba2de7f2678
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-11 12:09:26 -0600

    Simplified footer setup. https://app.asana.com/0/1108331976344856/1113629340275113

--- static-ui/footer.php
+++ static-ui/footer.php
@@ -4,25 +4,18 @@
 
 
 <main>
-
   <div class="container-fluid">
-  <div class="fixed-bottom row py-5 py-md-3 py-sm-1 text-white text-center bg-dark" id="footerRow">
-    <div class="col">
-      <div class="row">
-        <div class="col-md-3 col-sm-4 col-3 mx-auto py-md-2 py-sm-3">
-          <a href="" target=”_blank” class="fab fa-2x fa-facebook-square" alt="Facebook (Image courtesy of facebook.com)" title="Facebook (Image courtesy of facebook.com)" >
+   <div class="fixed-bottom d-flex space-around row py-5 py-md-3 py-sm-1 text-white text-center bg-dark" id="footerRow">
+
+          <a href="https://www.facebook.com/The-Arts-Haus-379835622596932/?modal=admin_todo_tour" target=”_blank” class="fab fa-2x fa-facebook-square" alt="Facebook (Image courtesy of facebook.com)" title="Facebook (Image courtesy of facebook.com)">
           </a>
-        </div>
-        <div class="col-md-3 col-sm-4 col-3 mx-auto py-md-2 py-sm-3">
-          <a href="" target=”_blank” class="fab fa-2x fa-twitter" alt="Twitter (Image courtesy of twitter.com)" title="Twitter (Image courtesy of twitter.com)" >
-          </a>
-        </div>
-      </div>
-      <footer>Copyright Art Haus 2019</footer>
+
+     <a href="" target=”_blank” class="fab fa-2x fa-twitter" alt="Twitter (Image courtesy of twitter.com)" title="Twitter (Image courtesy of twitter.com)"></a>
+
+      <footer>&copy Art Haus 2019</footer>
+
   	</div>
   </div>
-</div>
-
 </main>
 
 <!-- TODO: Add Facebook, Twitter, and any other links -->
