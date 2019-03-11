adda7f14ea832f3e28fe920ff60de80ed40256e6
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-11 13:07:55 -0600

    Formatted profile-view. https://app.asana.com/0/1108331976344856/1113629340275111

--- static-ui/profile-view.php
+++ static-ui/profile-view.php
@@ -14,39 +14,45 @@
 	</div>
 
 <div class="containter-fluid">
-	<div class="row bg-dark">
-  <div class="col-md-2 col-sm-2 col-1 mx-auto pb-3">
+<div class="row bg-dark">
+<div class="col-md-2 col-sm-2 col-1 mx-auto pb-3">
 
 	<div id="carousel" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner">
+			
 			<div class="carousel-item active">
 				<img class="d-block w-100" src="../sample-images/sample-image-1.jpg" alt="First slide">
-				  <div id="carouselCaption" class="carousel-caption d-md-block bg-dark">
-					<h5>Gallery Name</h5>
+				  	<div id="carouselCaption" class="carousel-caption d-md-block bg-dark">
+						<h5>Gallery Name</h5>
+					</div>
 			</div>
-			</div>
+			
 			<div class="carousel-item">
 				<img class="d-block w-100" src="../sample-images/sample-image-2.jpg" alt="Second slide">
-				<div id="carouselCaption" class="carousel-caption d-md-block bg-dark">
-					<h5>Gallery Name</h5>
+					<div id="carouselCaption" class="carousel-caption d-md-block bg-dark">
+						<h5>Gallery Name</h5>
+					</div>
 			</div>
-			</div>
+			
 			<div class="carousel-item">
 				<img class="d-block w-100" src="../sample-images/sample-image-3.jpg" alt="Third slide">
-				<div id="carouselCaption" class="carousel-caption d-md-block bg-dark ">
-					<h5>Gallery Name</h5>
+					<div id="carouselCaption" class="carousel-caption d-md-block bg-dark ">
+						<h5>Gallery Name</h5>
+					</div>
 			</div>
-		</div>
+			
 		<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
+		
 		<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
 		</a>
+		
+		</div>
 	</div>
-</div>
 </div>
 </div>
 </div>
