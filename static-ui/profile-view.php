7ec7dc18fd7e4b9eb79aaa05a32ae7676bc30656
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-11 13:11:16 -0600

    More formatting on profile-view. https://app.asana.com/0/1108331976344856/1113629340275111

--- static-ui/profile-view.php
+++ static-ui/profile-view.php
@@ -5,47 +5,47 @@
 <?php require_once("footer.php");?>
 
 <main>
-
+<!--JUMBOTRON:-->
 	<div class="jumbotron jumbotron-fluid">
 		<div class="container">
 			<h1 class="display-4">PROFILE NAME</h1>
 			<p class="lead">Profile Email<br/>Profile Website</p>
 		</div>
 	</div>
-
+<!--CAROUSEL:-->
 <div class="containter-fluid">
 <div class="row bg-dark">
 <div class="col-md-2 col-sm-2 col-1 mx-auto pb-3">
 
 	<div id="carousel" class="carousel slide" data-ride="carousel">
 		<div class="carousel-inner">
-			
+         <!--GALLERY 1:-->
 			<div class="carousel-item active">
 				<img class="d-block w-100" src="../sample-images/sample-image-1.jpg" alt="First slide">
 				  	<div id="carouselCaption" class="carousel-caption d-md-block bg-dark">
 						<h5>Gallery Name</h5>
 					</div>
 			</div>
-			
+         <!--GALLERY 2:-->
 			<div class="carousel-item">
 				<img class="d-block w-100" src="../sample-images/sample-image-2.jpg" alt="Second slide">
 					<div id="carouselCaption" class="carousel-caption d-md-block bg-dark">
 						<h5>Gallery Name</h5>
 					</div>
 			</div>
-			
+         <!--GALLERY 3:-->
 			<div class="carousel-item">
 				<img class="d-block w-100" src="../sample-images/sample-image-3.jpg" alt="Third slide">
 					<div id="carouselCaption" class="carousel-caption d-md-block bg-dark ">
 						<h5>Gallery Name</h5>
 					</div>
 			</div>
-			
+      <!--CONTROLS, PREVIOUS:-->
 		<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
-		
+      <!--CONTROLS, NEXT:-->
 		<a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
 			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
