0f4f4101aa56dc9b6786791fd235b33e6f842a1f
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-11 12:14:07 -0600

    Fixed footer spacing. https://app.asana.com/0/1108331976344856/1113629340275113

--- static-ui/footer.php
+++ static-ui/footer.php
@@ -5,14 +5,14 @@
 
 <main>
   <div class="container-fluid">
-   <div class="fixed-bottom d-flex space-around row py-5 py-md-3 py-sm-1 text-white text-center bg-dark" id="footerRow">
+   <div class="fixed-bottom d-flex justify-content-around row py-5 py-md-3 py-sm-1 text-white text-center bg-dark" id="footerRow">
 
           <a href="https://www.facebook.com/The-Arts-Haus-379835622596932/?modal=admin_todo_tour" target=”_blank” class="fab fa-2x fa-facebook-square" alt="Facebook (Image courtesy of facebook.com)" title="Facebook (Image courtesy of facebook.com)">
           </a>
 
      <a href="" target=”_blank” class="fab fa-2x fa-twitter" alt="Twitter (Image courtesy of twitter.com)" title="Twitter (Image courtesy of twitter.com)"></a>
 
-      <footer>&copy Art Haus 2019</footer>
+      <footer class="text-secondary">&copy Art Haus 2019</footer>
 
   	</div>
   </div>
