6a54303eb3685702dbe1506fc7419cdcd27d7f9a
William Isengard <william.isengard@gmail.com>
2019-03-12 15:22:12 -0600

    Updated gallery service. https://app.asana.com/0/1108331976344856/1113629340275104

--- ng/src/app/shared/services/gallery.service.ts
+++ ng/src/app/shared/services/gallery.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from "@angular/core";
 
-
 import {Gallery} from "../interfaces/gallery";
 import {Status} from "../interfaces/status";
 
@@ -8,28 +7,39 @@ import {Observable} from "rxjs/internal/Observable";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable ()
-export class ApiService {
+export class GalleryService {
 
-	constructor(protected http: HttpClient) {
-	}
+constructor(protected http: HttpClient) {
+}
 
 //define the API endpoint
-	//TODO: what url do we use? Maybe Brandon's since he has XXAMP
-	private galleryUrl = //"https://bootcamp-coders.cnm.edu/~gkephart/ng-demo7-backend/public_html/api/";
+private galleryUrl = "https://bootcamp-coders.cnm.edu/~gkephart/ng-demo7-backend/public_html/api/";
 
-	// call to the Gallery API and create the gallery in question
-	createGallery(gallery: Gallery): Observable<Status> {
-		return (this.http.post<Status>(this.galleryUrl, gallery));
-	}
+// call to the Gallery API and create the gallery in question
+createGallery(gallery: Gallery): Observable<Status> {
+return (this.http.post<Status>(this.galleryUrl, gallery));
+}
 
-	//call to API and delete a gallery
-	deleteGallery(gallery: Gallery): Observable<Status> {
-		return (this.http.delete<Status>(this.galleryUrl + galleryId));
-	}
+//call to API and delete a gallery
+deleteGallery(gallery: Gallery): Observable<Status> {
+return (this.http.delete<Status>(this.galleryUrl + galleryId));
+}
 
-	//*********************API CALLS -- PUT METHODS:**************************
-	editGallery(gallery: Gallery): Observable<Status> {
-		return (this.http.put<Status>(this.galleryUrl + galleryId));
-	}
+editGallery(gallery: Gallery): Observable<Status> {
+return (this.http.put<Status>(this.galleryUrl + galleryId));
+}
+
+//call the gallery API and get by gallery id
+getGalleryByGalleryId(galleryId : string) : Observable <Gallery[]> {
+return (this.http.get<Gallery[]>(this.galleryUrl, {params: new HttpParams().set("galleryId", galleryId)}));
+}
+//call the gallery API and get by gallery profile id
+getGalleryByGalleryProfileId(galleryId : string, galleryProfileId : string) : Observable <Gallery[]> {
+return (this.http.get<Gallery[]>(this.galleryUrl, {params: new HttpParams().set("galleryId", galleryId).set("galleryProfileId", galleryProfileId)}));
+}
+//call the gallery API and get by gallery name
+getGalleryByGalleryName(galleryName : string) : Observable <Gallery[]> {
+return (this.http.get<Gallery[]>(this.galleryUrl, {params: new HttpParams().set("galleryName", GalleryName)}));
+}
 
 }
