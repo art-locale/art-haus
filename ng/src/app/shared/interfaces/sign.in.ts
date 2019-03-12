8db2a07268e216cca2bc0c2c1b12877daa60a593
Brandon Huffman <bt_huffman@msn.com>
2019-03-12 09:52:06 -0600

    Created the interface .ts files for sign-in and sign-up https://app.asana.com/0/1108331976344856/1113629340275108 & https://app.asana.com/0/1108331976344856/1113629340275109

---
+++ ng/src/app/shared/interfaces/sign.in.ts
@@ -0,0 +1,11 @@
+export interface SignIn {
+	profileId: number,
+	profileActivationToken: number,
+	profileDate: string,
+	profileEmail: string,
+	profileLatitude: number,
+	profileLongitude: number,
+	profileName: string,
+	profilePassword: string,
+	profileWebsite: string
+}
---
+++ ng/src/app/shared/interfaces/sign.up.ts
@@ -0,0 +1,12 @@
+export interface SignUp {
+	profileId: number,
+	profileActivationToken: number,
+	profileDate: string,
+	profileEmail: string,
+	profileLatitude: number,
+	profileLongitude: number,
+	profileName: string,
+	profilePassword: string,
+	profilePasswordConfirm: string,
+	profileWebsite: string
+}
