cc5d86463e51cd044c0007f64df4a84e1c40615b
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-12 14:43:08 -0600

    Added empty service files.  https://app.asana.com/0/1108331976344856/1113629340275104?lg=1552399903616

---
+++ ng/src/app/shared/services/image.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from "@angular/core";
+
+//import {Profile} from "../interfaces/profile";
+import {Image} from "../interfaces/image";
+import {Gallery} from "../interfaces/gallery";
+import {Applaud} from "../interfaces/applaud";
+import {SignUp} from "../interfaces/sign.up";
+import {SignIn} from "../interfaces/sign.in";
+import {SignOut} from "../interfaces/sign.out";
+import {Status} from "../interfaces/status";
+//import {EarlGrey} from "../interfaces/earlGrey";
+
+import {Observable} from "rxjs/internal/Observable";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable ()
+export class ApiService {
+
+	constructor(protected http : HttpClient ) {}
