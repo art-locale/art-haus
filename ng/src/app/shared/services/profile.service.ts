d03ffcbc2ed43da23182e2d71c98621afdc09b00
William Isengard <william.isengard@gmail.com>
2019-03-13 14:36:23 -0600

    Corrected rxjs mapping

--- ng/src/app/shared/services/profile.service.ts
+++ ng/src/app/shared/services/profile.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from "@angular/core";
 import {Profile} from "../interfaces/profile";
 import {Status} from "../interfaces/status";
 
-import {Observable} from "rxjs/internal/Observable";
+import {Observable} from "rxjs";
 import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable ()
