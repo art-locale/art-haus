b6e980783ce65e6925f8b597847e16c2d67701ff
Brandon Huffman <bt_huffman@msn.com>
2019-03-13 13:36:39 -0600

    Minor adjustments to the sing-up ts https://app.asana.com/0/1108331976344856/1113629340275103

--- static-ui/sign-up.php
+++ static-ui/sign-up.php
@@ -6,38 +6,68 @@
 
 <main>
 
-  <div class="container text-center bg-secondary h-100">
+	<div class="container bg-secondary h-100">
 
-    <h1>Sign Up For Art Haus!</h1>
+		<h1 class="text-center">Sign Up For Art Haus!</h1>
 
-    <form>
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" placeholder="Name">
-      </div>
+		<form [formGroup]="signUpForm" class="form-control-lg" id="form" action="" method="post" (submit)="createSignUp();" novalidate>
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="name">Name</label>
+				<input type="text" class="d-flex justify-content-start form-control " id="name" placeholder="Name"/>
+			</div>
 
-      <div class="form-group">
-        <label for="email">Email Address</label>
-        <input type="email" class="form-control" id="email" placeholder="Email">
-      </div>
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="email">Email Address</label>
+				<input type="email" class="d-flex justify-content-start form-control" id="email" placeholder="Email"/>
+			</div>
 
-		 <div class="form-group">
-			 <label for="currentAddress">Current Address</label>
-			 <input type="text" class="form-control" id="currentAddress" placeholder="Current Address">
-		 </div>
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="addressLine1">Address Line 1</label>
+				<input type="text" class="d-flex justify-content-start form-control" id="addressLine1" placeholder="Street address, PO box, company name, c/o">
+			</div>
 
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" placeholder="Password">
-      </div>
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="addressLine2">Address Line 2</label>
+				<input type="text" class="d-flex justify-content-start form-control" id="addressLine2" placeholder="Apartment, suite, unit, building, floor, etc.">
+			</div>
 
-      <div class="form-group">
-        <label for="passwordConfirm">Confirm Password</label>
-        <input type="password" class="form-control" id="passwordConfirm" placeholder="Confirm Password">
-      </div>
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="city">City</label>
+				<input type="text" class="d-flex justify-content-start form-control" id="city" placeholder="City">
+			</div>
 
-      <button type="submit" class="btn btn-info">Submit</button>
-    </form>
-  </div>
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="state">State/Province/Region</label>
+				<input type="text" class="d-flex justify-content-start form-control" id="state" placeholder="State, province, region">
+			</div>
+
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="zip">ZIP/Postal Code</label>
+				<input type="text" class="d-flex justify-content-start form-control" id="zip" placeholder="Zip/Postal Code">
+			</div>
+
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="country">Country</label>
+				<input type="text" class="d-flex justify-content-start form-control" id="country" placeholder="Country">
+			</div>
+
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="website">Your Website</label>
+				<input type="password" class="d-flex justify-content-start form-control" id="website" placeholder="Your Website">
+			</div>
+
+			<div class="form-group">
+				<label for="password">Password</label>
+				<input type="password" class="form-control" id="password" placeholder="Password">
+			</div>
+
+			<div class="form-group">
+				<label class="d-flex justify-content-start" for="passwordConfirm">Confirm Password</label>
+				<input type="password" class="d-flex justify-content-start form-control" id="passwordConfirm" placeholder="Confirm Password">
+			</div>
+
+			<button type="submit" class="btn btn-info">Submit</button>
+		</form>
+	</div>
 
 </main>
