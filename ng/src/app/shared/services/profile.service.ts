979b0dc52f82af2a18474903b672f6b64772e857
William Isengard <william.isengard@gmail.com>
2019-03-13 14:15:37 -0600

    Removed duplicate function

--- ng/src/app/shared/services/profile.service.ts
+++ ng/src/app/shared/services/profile.service.ts
@@ -38,12 +38,6 @@ return(this.http.get<Profile>(this.profileUrl + id));
 getProfileByName(profileUserName: string) :Observable<Profile[]> {
 return(this.http.get<Profile[]>(this.profileUrl, {params: new HttpParams().set("profileUserName", profileUserName)}));
 }
-
-//call to the profile API and grab the corresponding profile by its email
-getProfileByProfileEmail(profileEmail: string) :Observable<Profile[]> {
-return(this.http.get<Profile[]>(this.profileUrl, {params: new HttpParams().set("profileEmail", profileEmail)}));
-}
-
 //reach out to the profile API and delete the profile in question
 deleteProfile(id : number) : Observable<Status> {
 return(this.http.delete<Status>(this.profileUrl + id));
