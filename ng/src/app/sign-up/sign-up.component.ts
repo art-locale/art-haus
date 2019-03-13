c9280f67e7ab4d2530dc34a2839fc8f92574d1a7
William Isengard <william.isengard@gmail.com>
2019-03-13 10:06:14 -0600

    Added sign-up.component.ts code

--- ng/src/app/sign-up/sign-up.component.ts
+++ ng/src/app/sign-up/sign-up.component.ts
@@ -6,56 +6,62 @@
 import {Component, OnInit, ViewChild,} from "@angular/core";
 import {Observable} from "rxjs/Observable"
 import {Router} from "@angular/router";
-import {Status} from "../shared/classes/status";
-import {SignUp} from "../shared/classes/sign.up";
+import {Status} from "../shared/interfaces/status";
+import {SignUp} from "../shared/interfaces/sign.up";
 import {setTimeout} from "timers";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {SignUpService} from "../shared/services/sign.up.service";
+import {SignIn} from "../shared/interfaces/sign.in";
 
 //declare $ for good old jquery
 declare let $: any;
 
 // set the template url and the selector for the ng powered html tag
-@Component({
-    template: require("./sign-up.component.html")
-})
 
+@Component({
+	template: require
+	("./sign-up.component.html"),
+	selector: "sign-up"
+})
 export class SignUpComponent implements OnInit{
 
-    signUpForm : FormGroup;
+	//
+	signUpForm : FormGroup;
+	status : Status = {status : null, message: null, type: null};
 
-    signUp: SignUp = new SignUp(null, null, null, null);
-    status: Status = null;
 
 
-    constructor(private formBuilder : FormBuilder, private router: Router, private signUpService: SignUpService) {}
+	constructor(private formBuilder : FormBuilder, private router: Router, private signUpService: SignUpService) {}
 
-    ngOnInit()  : void {
-        this.signUpForm = this.formBuilder.group({
-            profileUserName: ["", [Validators.maxLength(32), Validators.required]],
-            profileEmail: ["", [Validators.maxLength(128), Validators.required]],
-            profilePassword:["", [Validators.maxLength(48), Validators.required]],
-            profilePasswordConfirm:["", [Validators.maxLength(48), Validators.required]]
+	ngOnInit()  : void {
+		this.signUpForm = this.formBuilder.group({
+			atHandle: ["", [Validators.maxLength(32), Validators.required]],
+			email: ["", [Validators.maxLength(128), Validators.required]],
+			phoneNumber: ["", [Validators.maxLength(32)]],
+			password:["", [Validators.maxLength(48), Validators.required]],
+			passwordConfirm:["", [Validators.maxLength(48), Validators.required]]
 
-        });
+		});
 
-    }
+		this.status = {status : null, message: null, type: null}
 
-    createSignUp(): void {
+}
 
-        let signUp =  new SignUp(this.signUpForm.value.profileEmail, this.signUpForm.value.profileUserName, this.signUpForm.value.profilePassword, this.signUpForm.value.profilePasswordConfirm);
+	createSignUp(): void {
 
-        this.signUpService.createProfile(signUp)
-            .subscribe(status => {
-                this.status = status;
+		let signUp : SignUp = { profileAtHandle: this.signUpForm.value.atHandle, profileEmail: this.signUpForm.value.email, profilePassword: this.signUpForm.value.password, profilePasswordConfirm: this.signUpForm.value.passwordConfirm, profilePhone: this.signUpForm.value.phoneNumber};
 
-                if(this.status.status === 200) {
-                    alert(status.message);
-                    setTimeout(function() {
-                        $("#signUpForm").modal('hide');
-                    }, 500);
-                    this.router.navigate([""]);
-                }
-            });
-    }
+		this.signUpService.createProfile(signUp)
+			.subscribe(status => {
+				this.status = status;
+
+				if(this.status.status === 200) {
+					alert(status.message);
+					setTimeout(function() {
+						$("#signUp-modal").modal('hide');
+					}, 500);
+					this.router.navigate([""]);
+				}
+			});
+	}
 }
