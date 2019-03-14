cc0d11486efa58c17b593d841e001cf7533fd2cb
William Isengard <william.isengard@gmail.com>
2019-03-14 13:01:54 -0600

    Created initial pages for signed in profile component

--- ng/src/app/profile-view/profile-view.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component, OnInit, ViewChild,} from "@angular/core";
-
-@Component({
-	templateUrl: "./profile-view.component.html"
-})
-
-export class ProfileViewComponent {
-
-}
---
+++ ng/src/app/signed-in-profile-view/signed-in-profile-view.component.ts
@@ -0,0 +1,9 @@
+import {Component, OnInit, ViewChild,} from "@angular/core";
+
+@Component({
+	templateUrl: "./signed-in-profile-view.component.html"
+})
+
+export class SignedInProfileViewComponent {
+
+}
