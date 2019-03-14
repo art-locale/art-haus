fb8135c56e26097af8fc3a7208ef82fccb318621
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-14 10:56:25 -0600

    Roughed out splash files.  https://app.asana.com/0/1108331976344856/1113629340275077

--- ng/src/app/splash/splash.component.ts
+++ ng/src/app/splash/splash.component.ts
@@ -1,7 +1,27 @@
+
 import {Component, OnInit} from "@angular/core";
+//import {User} from "../shared/interfaces/profile";
+import {ApiService} from "../shared/services/api.service";
+import {Router} from "@angular/router";
 
 @Component({
 	templateUrl: "./splash.component.html"
 })
 
-export class SplashComponent {}
+
+export class SplashComponent implements OnInit{
+	users: User[] = [];
+
+	constructor(protected userService: ApiService, private router: Router) {}
+
+
+	ngOnInit():void {
+		this.userService.getAllUsers()
+			.subscribe(users => this.users = users);
+	}
+
+	getDetailedView(user : User) : void {
+		this.router.navigate(["/detailed-user/", user.userId]);
+	}
+
+}
