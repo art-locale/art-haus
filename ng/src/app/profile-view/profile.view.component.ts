e72bb8b3a464912b3b052c1e62324dd18edcde90
Brandon Huffman <bt_huffman@msn.com>
2019-03-15 09:45:29 -0600

    Profile-view improvements https://app.asana.com/0/1108331976344856/1113629340275096

--- ng/src/app/profile-view/profile.view.component.ts
+++ ng/src/app/profile-view/profile.view.component.ts
@@ -1,27 +1,32 @@
 import {Component, OnInit} from "@angular/core";
-import {GalleryService} from "../shared/services/gallery.service";
-import {Gallery} from "../shared/interfaces/gallery";
+// import {Router} from "@angular/router";
+import {ProfileService} from "../shared/services/profile.service";
+import {Profile} from "../shared/interfaces/profile";
 // import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 // import {repeat} from "rxjs/operators";
 import {Status} from "../shared/interfaces/status";
+// import {AuthService} from "../shared/services/auth.service";
 // import {el} from "@angular/platform-browser/testing/src/browser_util";
+// import {SessionService} from "../shared/services/session.service";
 
 @Component({
-	templateUrl: "gallery.view.component.html"
+	templateUrl: "./profile.view.component.html",
+	selector: 'profile-view-component'
 })
 
-export class GalleryViewComponent implements OnInit{
+export class ProfileViewComponent implements OnInit{
 	//create state variable to house all data
-	gallery : Gallery[] = [];
-	status: Status = new Status(null, null, null);
-	constructor(private galleryService: GalleryService) {}
+	profile : Profile [] = [];
+	status: Status = (status : null, message: null, type: null};
+
+	constructor(private profileService: ProfileService) {}
 
 	//call onInit above to work (fulfill the contract)
 	ngOnInit(): void {
-		this.loadGallery();
+		this.loadProfile();
 	}
 
-	loadGallery() {
-		this.galleryService.getGalleryByGalleryName().subscribe(reply => this.gallery = reply);
+	loadProfile() {
+		this.profileService.getProfileByProfileId().subscribe(reply => this.profile = reply);
 	}
 }
