cc6f0639a5c29a25aa70214b5ddc9a0149e0e306
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-16 13:18:13 -0600

    Added profile view declaration to module.  https://app.asana.com/0/1108331976344856/1113629340275077

--- ng/src/app/app.module.ts
+++ ng/src/app/app.module.ts
@@ -6,10 +6,11 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {allAppComponents, providers, routing} from "./app.routes"
 import { AppComponent } from './app.component';
 import { SignInComponent} from "./sign-in.component";
+import {ProfileViewComponent} from "./profile-view/profile.view.component";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpClientModule, routing, ReactiveFormsModule],
-  declarations: [ ...allAppComponents, AppComponent, SignInComponent],
+  declarations: [ ...allAppComponents, AppComponent, SignInComponent, ProfileViewComponent],
   bootstrap:    [ AppComponent ],
   providers: [providers]
 })
