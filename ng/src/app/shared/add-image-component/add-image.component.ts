ffd605acbfe1ade1b95dafac7af742aed5a74345
Brandon Huffman <bt_huffman@msn.com>
2019-03-18 13:55:04 -0600

    retreiving changes made from billy @gkephart

--- ng/src/app/shared/add-image-component/add-image.component.ts
+++ ng/src/app/shared/add-image-component/add-image.component.ts
@@ -2,18 +2,28 @@ import {Component, Input, OnInit} from '@angular/core';
 import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {FileUploader} from "ng2-file-upload";
 import { CookieService } from 'ngx-cookie';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
+<<<<<<< HEAD
 		templateUrl: "./add-image.component.html",
+=======
+		templateUrl: "./add-image.component.html"
+>>>>>>> retreiving changes made from billy @gkephart
 	}
 )
 
 export class AddImageComponent implements OnInit{
 
+	imageCreateForm: FormGroup;
+
 	uploader: FileUploader = null;
-	constructor(private activeModalService: NgbActiveModal,private cookieService: CookieService){}
+	constructor(private activeModalService: NgbActiveModal,private cookieService: CookieService, private formBuilder: FormBuilder){}
+
+
 
 	ngOnInit(): void {
+		this.imageCreateForm = this.formBuilder.group({imageTitle: ["", Validators.maxLength(32), Validators.required]});
 		this.uploader = new FileUploader(
 			{
 				itemAlias: 'image',
