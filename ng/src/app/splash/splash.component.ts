eb62a287f603be6cb4287cd6de08dbae3bcfcbcc
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-18 13:29:12 -0600

    Added address property to splash component.   https://app.asana.com/0/1108331976344856/1113629340275112

--- ng/src/app/splash/splash.component.ts
+++ ng/src/app/splash/splash.component.ts
@@ -29,7 +29,8 @@ export class SplashComponent implements OnInit {
 			profileLongitude: null,
 			profileName: null,
 			profilePassword: null,
-			profileWebsite: null};
+			profileWebsite: null,
+			profileAddress: null};
 
 		galleryId = {galleryId: "12f2ed5f-9341-4450-9174-24eaadd6e3e2"};
 		gallery: Gallery = {galleryId: null, galleryProfileId: null, galleryDate: null, galleryName: null};
