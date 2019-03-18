921145475c5c2279c1d27e3ba989ff4f1d8170d8
Brandon Huffman <bt_huffman@msn.com>
2019-03-18 11:11:46 -0600

    Image component.  https://app.asana.com/0/1108331976344856/1113629340275102

--- ng/src/app/shared/add-image-component/add-image.component.ts
+++ ng/src/app/shared/add-image-component/add-image.component.ts
@@ -1,61 +1,33 @@
-/*
- this component is for signing in to use the site.
- */
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {FileUploader} from "ng2-file-upload";
+import { CookieService } from 'ngx-cookie';
 
-//import needed modules for the sign-in component
-//TODO Removed ViewChild and Input from the following import
-import {Component, OnInit} from "@angular/core";
-import {Router} from "@angular/router";
-import {NgbActiveModal, NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Status} from "../interfaces/status";
-import {ImageService} from "../services/image.service";
-import {SessionService} from "../services/session.service";
-import {Image} from "../interfaces/image";
-
-
-// set the template url and the selector for the ng powered html tag
 @Component({
-	templateUrl:"./add-image.component.html",
-	selector: "add-image-form"
-})
-
-export class AddImageComponent implements OnInit {
-
-	addImageForm: FormGroup;
-
-
-	status: Status = null;
-
-	constructor(public activeModal: NgbActiveModal, private formBuilder: FormBuilder, private router: Router, private ImageService: ImageService, private sessionService: SessionService) {
+		selector: 'add-image-form',
+		templateUrl: "./add-image.component.html",
 	}
+)
+
+export class AddImageComponent implements OnInit{
+
+	uploader: FileUploader = null;
+	constructor(private activeModalService: NgbActiveModal,private cookieService: CookieService){}
 
 	ngOnInit(): void {
-		this.addImageForm = this.formBuilder.group({
-			title: ["", [Validators.maxLength(128), Validators.required]]
-		});
-
-		this.status = {status: null, message: null, type: null}
-
+		this.uploader = new FileUploader(
+			{
+				itemAlias: 'image',
+				url: './api/image/',
+				additionalParameter: {}
+			}
+		);
 	}
 
-	editImage(): void {
-		let	image: Image = {imageTitle: this.addImageForm.value.title};
-		this.ImageService.editImage(Image)
-			.subscribe(status => {
-				this.status = status;
+	closeModalButton(){
+		this.activeModalService.dismiss("Cross click")
+	}
 
-				if(this.status.status === 200) {
-					alert("Thanks for adding an image!")
-					this.addImageForm.reset();
-					location.reload();
-
-					this.router.navigate(["signed-in-homeview.php"])
-				}
-			});
+	submitImage(){
 	}
 }
-
-
-
