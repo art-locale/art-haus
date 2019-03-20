f76e1dd3eca5318c9cba02b97e4ef2831344a21f
Brandon Huffman <bt_huffman@msn.com>
2019-03-20 16:52:14 -0600

    Deleting a gallery and then related profile. https://app.asana.com/0/1108331976344856/1113629340275102

--- sql/ddl-art-locale.sql
+++ sql/ddl-art-locale.sql
@@ -14,9 +14,9 @@ ALTER DATABASE arthaus CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 -- To delete a specific image, notice all letters must be lower case.
 DELETE FROM image WHERE imageId = UNHEX("6e3318177d0441148f1c39294c5117e1");
 -- To delete a specific gallery, notice all letters must be lower case. NOTE: Must delete the gallery then too
-DELETE FROM gallery WHERE galleryProfileId = UNHEX("a28354a1f093445eb01e178fac7b05aa");
+DELETE FROM gallery WHERE galleryProfileId = UNHEX("820cd0722bef40fe9099eb46e726869a");
 -- To delete a specific profile, notice all letters must be lower case. NOTE: Must delete the gallery then too
-DELETE FROM profile WHERE profileName = "Brandonius Maximus";
+DELETE FROM profile WHERE profileName = "Sebastian Traverez";
 
 -- this creates the profile entity
 CREATE TABLE profile (
